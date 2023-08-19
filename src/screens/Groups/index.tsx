941c995b3a4fc4424747bcc5ee6f019bef9eb753
Thiago Leite <leite.tjs@gmail.com>
2023-08-19 19:34:55 -0300

    feat: list groups with flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,29 @@
 import { Header } from '@components/Header'
 import { Container } from './styles'
 import { HighLight } from '@components/HighLight'
+import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<Array<string>>([
+    'Galera da Rocket',
+    'Amigos',
+    'Família',
+  ])
+
   return (
     <Container>
       <Header />
-
       <HighLight title="Turmas" subTitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Fundão" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
