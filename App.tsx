2089da08dc0472982f491ee07a15b7ca09c71cb0
Thiago Leite <leite.tjs@gmail.com>
2023-08-19 18:11:06 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular as roboto400Regular,
+  Roboto_700Bold as roboto700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { Groups } from '@screens/Groups'
+import theme from '@theme/index'
+import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    roboto400Regular,
+    roboto700Bold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  )
+}
